ec7eee800cccbc27a094aefff607b559a56126ca
KayaanT <kayaan.tharani@gmail.com>
2018-11-04 12:35:23 -0600

    Button

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-alert("Hello World");
+function computer() {
+    document.body.style.background = "#FF0000 url('http://pluspng.com/img-png/computer-hd-png-computer-desktop-pc-png-1262.png') " +
+        "no-repeat right top";
+}

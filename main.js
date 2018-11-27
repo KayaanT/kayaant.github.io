f622893c3fe2e0de1e3813b10b0f1b2056e79523
KayaanT <kayaan.tharani@gmail.com>
2018-11-27 11:48:28 -0600

    New Website

--- main.js
+++
@@ -1,4 +0,0 @@
-function computer() {
-    document.body.style.background = "#FF0000 url('http://pluspng.com/img-png/computer-hd-png-computer-desktop-pc-png-1262.png') " +
-        "no-repeat right top";
-}

c4a07a5b0da7d673cf9fffde498cc82dfc64fc12
KayaanT <kayaan.tharani@gmail.com>
2021-10-08 12:08:57 -0600

    Created grid demo

---
+++ grid/sketch.js
@@ -0,0 +1,46 @@
+// Grid Demo
+let gridSize = 30;
+let grid;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = createEmpty2DArray(gridSize, gridSize);
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function createEmpty2DArray(rows, cols) {
+  let grid = [];
+  for (let y = 0; y < rows; y++) {
+    grid.push([]);
+    for (let x = 0; x < cols; x++) {
+      if (random(100) > 50) {
+        grid[y].push(0);    
+      }
+      else {
+        grid[y].push(1);    
+      }
+    }
+  }
+  return grid;
+}
+
+function displayGrid() {
+  let cellWidth = width/gridSize;
+  let cellHeight = height/gridSize;
+
+  for (let y = 0; y < gridSize; y++) {
+    for (let x = 0; x < gridSize; x++) {
+      if (grid[y][x] === 0) {
+        fill(255);
+      }
+      else {
+        fill(0);
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+}

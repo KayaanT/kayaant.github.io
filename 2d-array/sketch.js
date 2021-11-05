bb62fd0a21505dfbe0fe7b8e572f6ac7c648a9ce
KayaanT <kayaan.tharani@gmail.com>
2021-11-04 21:15:06 -0600

    efficiencies

--- 2d-array/sketch.js
+++ 2d-array/sketch.js
@@ -3,14 +3,15 @@
 // Date
 //
 // Extra for Experts:
-// Created an AI which makes different moves every time
+// Created an AI which makes different moves every time, but with intelligence
 
 
 // to do:
+// REWRITE CODE/FUNCTIONS
 // loop to check x and o at the same time
 // fix function calls to work in order
 // create custom shapes for x and o (white) or to click
-// cursor change to x or o when in right spot
+// function to check wher emouse x is, then link or cross cursor
 // decrease size of grid
 // strikethrough when won
 
@@ -38,7 +39,7 @@ function setup() {
 }
 
 function draw() {
-  cursor('pointer');
+  cursor('pointer'); // change this into a function maybe 
   background("black");
   drawGrid();
   displayXandO();
@@ -175,52 +176,50 @@ function writeScores() {
 function autoMove() {
   if (!xTurn) {
     // check if o can win 
+    // diagonal top left to bottm right
+    if (grid[0][0] === 2 && grid[1][1] === 2 && grid[2][2] === 0 || grid[0][0] === 2 && grid[1][1] === 0 && grid[2][2] === 2 || grid[0][0] === 0 && grid[1][1] === 2 && grid[2][2] === 2) {
+      for (let j = 0; j < 3; j++) {
+        grid[j][j] = 2;  
+      }
+    }
+    // diagonal top right to bottm left
+    if (grid[0][2] === 2 && grid[1][1] === 2 && grid[2][0] === 0 || grid[0][2] === 2 && grid[1][1] === 0 && grid[2][0] === 2 || grid[0][2] === 0 && grid[1][1] === 2 && grid[2][0] === 2) {
+      for (let j = 0; j < 3; j++) {
+        if (grid[j][2-j] === 0) {
+          grid[j][2-j] = 2;
+        }
+      }
+    }
+
     for (let i = 0; i < 3; i++) { 
       // rows
       if (grid[i][0] === 2 && grid[i][1] === 2 && grid[i][2] !== 1 || grid[i][2] === 2 && grid[i][1] === 2 && grid[i][0] !== 1 || grid[i][2] === 2 && grid[i][0] === 2 && grid[i][1] !== 1) {
-        grid[i][0] = 2;
-        grid[i][1] = 2;
-        grid[i][2] = 2;
+        for (let j = 0; j < 3; j++) {
+          grid[i][j] = 2;
+        }
         xTurn = true;
         checkThreeInARow();
         checkTie();
         return;
       }
       // columns
-      if (grid[0][i] === 2 && grid[1][i] === 2 && grid[2][i] !== 1|| grid[0][i] === 2 && grid[2][i] === 2 && grid[1][i] !== 1|| grid[1][i] === 2 && grid [2][i] === 2 && grid[0][i] !== 1) {
-        grid[0][i] = 2;
-        grid[1][i] = 2;
-        grid[2][i] = 2;
+      if (grid[0][i] === 2 && grid[1][i] === 2 && grid[2][i] !== 1 || grid[0][i] === 2 && grid[2][i] === 2 && grid[1][i] !== 1|| grid[1][i] === 2 && grid [2][i] === 2 && grid[0][i] !== 1) {
+        for (let j = 0; j < 3; j++) {
+          grid[j][i] = 2;
+        }
         xTurn = true;
         checkThreeInARow();
         checkTie();
         return;
       }
 
-      // diagonal top left to bottm right
-      if (grid[0][0] === 2 && grid[1][1] === 2 && grid[2][2] === 0 || grid[0][0] === 2 && grid[1][1] === 0 && grid[2][2] === 2 || grid[0][0] === 0 && grid[1][1] === 2 && grid[2][2] === 2) {
-        grid[0][0] = 2;
-        grid[1][1] = 2;
-        grid[2][2] = 2;
-      }
-      // diagonal top right to bottm left
-      if (grid[0][2] === 2 && grid[1][1] === 2 && grid[2][0] === 0 || grid[0][2] === 2 && grid[1][1] === 0 && grid[2][0] === 2 || grid[0][2] === 0 && grid[1][1] === 2 && grid[2][0] === 2) {
-        grid[0][2] = 2;
-        grid[1][1] = 2;
-        grid[2][0] = 2;
-      }
-
       // check if x can win
       //rows
       if (grid[i][0] === 1 && grid[i][1] === 1 && grid[i][2] === 0 || grid[i][2] === 1 && grid[i][1] === 1 && grid[i][0] === 0 || grid[i][2] === 1 && grid[i][0] === 1 && grid[i][1] === 0) {
-        if (grid[i][0] === 0) {
-          grid[i][0] = 2;
-        }
-        if (grid[i][1] === 0) {
-          grid[i][1] = 2;
-        }
-        if (grid[i][2] === 0) {
-          grid[i][2] = 2;
+        for (let j = 0; j < 3; j++) {
+          if (grid[i][j] === 0) {
+            grid[i][j] = 2;
+          }
         }
         xTurn = true;
         checkThreeInARow();
@@ -230,53 +229,41 @@ function autoMove() {
 
       //columns
       if (grid[0][i] === 1 && grid[1][i] === 1 && grid[2][i] === 0 || grid[0][i] === 1 && grid[2][i] === 1 && grid[1][i] === 0|| grid[1][i] === 1 && grid [2][i] === 1 && grid[0][i] === 0) {
-        if (grid[0][i] === 0) {
-          grid[0][i] = 2;
-        }
-        if (grid[1][i] === 0) {
-          grid[1][i] = 2;
-        }
-        if (grid[2][i] === 0) {
-          grid[2][i] = 2;
+        for (let j = 0; j < 3; j++) {
+          if (grid[j][i] === 0) {
+            grid[j][i] = 2;
+          }
         }
         xTurn = true;
         checkThreeInARow();
         checkTie();
         return;
       }
+    }
 
-      // diagonal
-      if (grid[0][0] === 1 && grid[1][1] === 1 && grid[2][2] === 0 || grid[0][0] === 1 && grid[1][1] === 0 && grid[2][2] === 1 || grid[0][0] === 0 && grid[1][1] === 1 && grid[2][2] === 1) {
-        if (grid[0][0] === 0) {
-          grid[0][0] = 2;
+    // diagonal top left to bottom right
+    if (grid[0][0] === 1 && grid[1][1] === 1 && grid[2][2] === 0 || grid[0][0] === 1 && grid[1][1] === 0 && grid[2][2] === 1 || grid[0][0] === 0 && grid[1][1] === 1 && grid[2][2] === 1) {
+      for (let j = 0; j < 3; j++) {
+        if (grid[j][j] === 0) {
+          grid[j][j] = 2;
         }
-        if (grid[1][1] === 0) {
-          grid[1][1] = 2;
-        }
-        if (grid[2][2] === 0) {
-          grid[2][2] = 2;
-        }
-        xTurn = true;
-        checkThreeInARow();
-        checkTie();
-        return;
       }
-      // diagonal top right to bottm left
-      if (grid[0][2] === 1 && grid[1][1] === 1 && grid[2][0] === 0 || grid[0][2] === 1 && grid[1][1] === 0 && grid[2][0] === 1 || grid[0][2] === 0 && grid[1][1] === 1 && grid[2][0] === 1) {
-        if (grid[0][2] === 0) {
-          grid[0][2] = 2;
+      xTurn = true;
+      checkThreeInARow();
+      checkTie();
+      return;
+    }
+    // diagonal top right to bottm left
+    if (grid[0][2] === 1 && grid[1][1] === 1 && grid[2][0] === 0 || grid[0][2] === 1 && grid[1][1] === 0 && grid[2][0] === 1 || grid[0][2] === 0 && grid[1][1] === 1 && grid[2][0] === 1) {
+      for (let j = 0; j < 3; j++) {
+        if (grid[j][2-j] === 0) {
+          grid[j][2-j] = 2;
         }
-        if (grid[1][1] === 0) {
-          grid[1][1] = 2;
-        }
-        if (grid[2][0] === 0) {
-          grid[2][0] = 2;
-        }
-        xTurn = true;
-        checkThreeInARow();
-        checkTie();
-        return;
       }
+      xTurn = true;
+      checkThreeInARow();
+      checkTie();
+      return;
     }
 
     // if none of the above is possible, then place an o in a square
@@ -291,7 +278,7 @@ function autoMove() {
       {y: 1, x: 2},
       {y: 2, x: 1},
     ];
-    spotsToFill = shuffle(spotsToFill);
+    spotsToFill = shuffle(spotsToFill); // randomize moves
 
     for (let pair of spotsToFill) {
       if (grid[pair.y][pair.x] === 0) {
